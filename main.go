e40c43647237d2af00d5a1fb889af1fd689acb44
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-11-07 00:11:54 +0100

    add the database connection

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
     "net/http"
     "os"
 	"github.com/joho/godotenv"
+	db "chi-api-example/database"
 	"chi-api-example/routers"
 )
 
 func main() {
 	godotenv.Load()
+
+	conn := db.InitDbConn()
+	defer conn.Close(context.Background())
 
     router := routers.InitRouter()
     HTTP_PORT := os.Getenv("HTTP_PORT")
--- routers/router.go
+++ routers/router.go
@@ -1,36 +1,80 @@
 package routers
 
 import (
+	"context"
 	"fmt"
 	"encoding/json"
 	"net/http"
 	"strconv"
 	"github.com/go-chi/chi/v5"
     "github.com/go-chi/chi/v5/middleware"
+    db "chi-api-example/database"
     "chi-api-example/models"
 )
+
 func InitRouter() *chi.Mux {
 	router := chi.NewRouter()
     router.Use(middleware.Logger)
 
-    router.Get("/api/movies", func(res http.ResponseWriter, req *http.Request) {
-    	fmt.Println("Get all movies")
-        res.Write([]byte("Get all movies"))
-    })
-
-    router.Post("/api/movies", func(res http.ResponseWriter, req *http.Request) {
-    	movie := models.Movie{}
-		json.NewDecoder(req.Body).Decode(&movie)
-
-		fmt.Println("Post movie with id", movie.Id)
-		fmt.Println("Post movie with title", movie.Title)
-		fmt.Println("Post movie with release date", movie.ReleaseDate)
-    })
-
-    router.Delete("/api/movies/{movieId}", func(res http.ResponseWriter, req *http.Request) {
-   		movieId, _ :=  strconv.Atoi(chi.URLParam(req, "movieId"))
-     	fmt.Println("Delete movie with id", movieId)
-    })
+    router.Get("/api/movies", getAllMovies)
+    router.Post("/api/movies", saveMovie)
+    router.Delete("/api/movies/{movieId}", deleteMovie)
 
     return router
+}
+
+func getAllMovies(res http.ResponseWriter, req *http.Request) {
+	queries := db.New(db.GetDbConnection())
+
+	fmt.Println("Get all movies")
+	results, err := queries.GetMovies(context.Background())
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(res).Encode(err)
+	}
+
+	movies := make([]models.Movie, len(results))
+    for i, result := range results {
+        movies[i] = convertDbMovieToModelsMovie(result)
+    }
+    res.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(res).Encode(movies)
+}
+
+func saveMovie(res http.ResponseWriter, req *http.Request) {
+	queries := db.New(db.GetDbConnection())
+
+	movie := models.Movie{}
+	json.NewDecoder(req.Body).Decode(&movie)
+
+	fmt.Println("Post movie with id", movie.Id)
+	fmt.Println("Post movie with title", movie.Title)
+	fmt.Println("Post movie with release date", movie.ReleaseDate)
+	queries.SaveMovie(context.Background(), db.SaveMovieParams{
+		ID: movie.Id,
+		Title: movie.Title,
+		ReleaseDate: movie.ReleaseDate,
+	})
+}
+
+func deleteMovie(res http.ResponseWriter, req *http.Request) {
+	queries := db.New(db.GetDbConnection())
+
+	movieId, err :=  strconv.Atoi(chi.URLParam(req, "movieId"))
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(res).Encode(err)
+	}
+
+	fmt.Println("Delete movie with id", movieId)
+	queries.DeleteMovie(context.Background(), int32(movieId))
+}
+
+func convertDbMovieToModelsMovie(dbMovie db.Movie) (models.Movie) {
+	movie := models.Movie{
+		Id: dbMovie.ID,
+		Title: dbMovie.Title,
+		ReleaseDate: dbMovie.ReleaseDate,
+	}
+	return movie
 }
